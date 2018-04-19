2cfd0f783e02758810362e96cc061d51a1965608
beepscore <support@beepscore.com>
2018-04-18 22:08:48 -0700

    Add calculateAmortization

--- js/main.js
+++ js/main.js
@@ -9,6 +9,32 @@ let calculateMonthlyPayment = function (principal, years, rate) {
     return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
+// use arrow function =>
+let calculateAmortization = (principal, years, rate) => {
+    // ecmascript 5 "var" is scoped to entire function, even if within a {} block
+    // ecmascript 6 "let" is scoped to block {}
+    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
+    let balance = principal;
+    let amortization = [];
+    for (let y=0; y<years; y++) {
+        // interest payment for year y
+        let interestY = 0;
+        // principal payment for year y
+        let principalY = 0;
+        for (let m=0; m<12; m++) {
+            // interest payment for month m
+            let interestM = balance * monthlyRate;
+            // principal payment for month m
+            let principalM = monthlyPayment - interestM;
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({principalY, interestY, balance});
+    }
+    return {monthlyPayment, monthlyRate, amortization};
+};
+
 document.getElementById('calcBtn').addEventListener('click', function () {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
