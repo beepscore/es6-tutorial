a6149d7b0d738be39b598fc588962f29b18fdac6
beepscore <support@beepscore.com>
2018-04-19 22:03:06 -0700

    Ran npm run webpack
    > es6-tutorial@1.0.0 webpack /Users/stevebaker/Documents/projects/javascriptProjects/NodeProjects/es6-tutorial
    > webpack
    
    Hash: 89bbc0ae08aba1a5ff87
    Version: webpack 3.10.0
    Time: 409ms
                 Asset     Size  Chunks             Chunk Names
        main.bundle.js  5.86 kB       0  [emitted]  main
    main.bundle.js.map   7.1 kB       0  [emitted]  main
       [0] ./js/main.js 1.31 kB {0} [built]
       [1] ./js/mortgage.js 1.94 kB {0} [built]

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -70,8 +70,45 @@
 "use strict";
 
 
+var _mortgage = __webpack_require__(1);
+
+var mortgage = _interopRequireWildcard(_mortgage);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+
+    // use destructuring assignment to assign multiple variables
+    // use module "mortgage"
+
+    var _mortgage$calculateAm = mortgage.calculateAmortization(principal, years, rate),
+        monthlyPayment = _mortgage$calculateAm.monthlyPayment,
+        monthlyRate = _mortgage$calculateAm.monthlyRate,
+        amortization = _mortgage$calculateAm.amortization;
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+    // log is visible in browser / developer console
+    amortization.forEach(function (month) {
+        return console.log(month);
+    });
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 // use arrow function =>
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+var calculateMonthlyPayment = exports.calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     // ecmascript 5 "var" is scoped to entire function, even if within a {} block
     // ecmascript 6 "let" is scoped to block {}
     var monthlyRate = 0;
@@ -82,7 +119,7 @@ var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years,
     return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
 };
 
-var calculateAmortization = function calculateAmortization(principal, years, rate) {
+var calculateAmortization = exports.calculateAmortization = function calculateAmortization(principal, years, rate) {
     // ecmascript 5 "var" is scoped to entire function, even if within a {} block
     // ecmascript 6 "let" is scoped to block {}
     var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
@@ -109,26 +146,6 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
     }
     return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
 };
-
-document.getElementById('calcBtn').addEventListener('click', function () {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-
-    // use destructuring assignment to assign multiple variables
-
-    var _calculateAmortizatio = calculateAmortization(principal, years, rate),
-        monthlyPayment = _calculateAmortizatio.monthlyPayment,
-        monthlyRate = _calculateAmortizatio.monthlyRate,
-        amortization = _calculateAmortizatio.amortization;
-
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-    // log is visible in browser / developer console
-    amortization.forEach(function (month) {
-        return console.log(month);
-    });
-});
 
 /***/ })
 /******/ ]);
