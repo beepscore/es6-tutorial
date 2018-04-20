deea049a223f6a89545f1d6a12dbadab99059d36
beepscore <support@beepscore.com>
2018-04-19 20:54:38 -0700

    Revise calcBtn event listener

--- js/main.js
+++ js/main.js
@@ -35,14 +35,15 @@ let calculateAmortization = (principal, years, rate) => {
     return {monthlyPayment, monthlyRate, amortization};
 };
 
-document.getElementById('calcBtn').addEventListener('click', function () {
+document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
 
-    // use destructuring assignment to assign monthlyPayment and monthlyRate
-    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
+    // use destructuring assignment to assign multiple variables
+    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+    amortization.forEach(month => console.log(month));
 });
